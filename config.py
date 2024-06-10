96453230bed1c22bccf78f49209a3e105c84fd32
Hisham <htemmar@umich.edu>
2024-06-10 11:30:31 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# get cwd for making the folders - so that all the data should generate/get saved into the right places
+cwd = os.getcwd()
+notesdir = os.path.join(cwd,'joker_notes_firstpass')
+datapath = 'Z:\Data\Monkeys'
---
+++ extractdates.py
@@ -0,0 +1,18 @@
+import csv
+import os
+import re
+import config
+import datetime
+
+noteLocation = os.path.join(config.notesdir)
+
+noteList = os.listdir(noteLocation)
+
+extractedDates = [re.split('NapierNotes|\.txt', note)[1] for note in noteList]
+extractedDates.sort()
+extractedDates = [[datetime.datetime.strptime(date, '%Y%m%d').date().isoformat()] for date in extractedDates]
+
+with open('firstpass_dates.csv', 'w', newline='') as f:
+    write = csv.writer(f)
+    write.writerow(['Date', 'Runs'])
+    write.writerows(extractedDates)
